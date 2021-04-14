61e3176da654bd83e093f207b0b34ba6bd1a1fcb
barbara zujewska <barbara@zujewska.com>
2021-04-15 00:20:37 +0200

    linting

--- src/game.js
+++ src/game.js
@@ -1,17 +1,17 @@
-import WorldMap from './worldMap'
+import WorldMap from './worldMap';
 
 const directions = {
-  RIGHT: "right",
-  LEFT: "left",
-  UP: "up",
-  DOWN: "down",
-}
+  RIGHT: 'right',
+  LEFT: 'left',
+  UP: 'up',
+  DOWN: 'down',
+};
 
- export default class Game {
-  constructor (width, height, renderingContext) {   
+export default class Game {
+  constructor(width, height, renderingContext) {
     this.width = width;
     this.height = height;
-    this.renderingContext = renderingContext
+    this.renderingContext = renderingContext;
     this.lastTime = (new Date()).getTime();
     this.setStartingState = this.gameInit();
     this.gameLoop = this.gameLoop.bind(this);
@@ -21,7 +21,7 @@ const directions = {
 
   gameInit() {
     this.worldMap = new WorldMap(this.width, this.height, this.renderingContext);
-    this.rate = 1000/3;
+    this.rate = 1000 / 3;
     this.snakeLength = 3;
     this.snakeHeadCoordinates = [];
     this.snakeDirection = directions.RIGHT;
@@ -29,108 +29,112 @@ const directions = {
     this.placeApple();
     this.worldMap.draw();
   }
+
   gameLoop() {
     window.requestAnimationFrame(this.gameLoop);
-    
+
     const currentTime = (new Date()).getTime();
-    let delta = (currentTime-this.lastTime);
-    
-    if(delta > this.rate) {
-      switch (this.snakeDirection){
+    const delta = (currentTime - this.lastTime);
+
+    if (delta > this.rate) {
+      switch (this.snakeDirection) {
         case directions.RIGHT:
           this.snakeHeadCoordinates[0]++;
-          this.snakeHeadCoordinates[0] == this.width ? 
-            this.snakeHeadCoordinates[0] = 0 :
-            null;
+          this.snakeHeadCoordinates[0] === this.width
+            ? this.snakeHeadCoordinates[0] = 0
+            : null;
           break;
         case directions.LEFT:
           this.snakeHeadCoordinates[0]--;
-          this.snakeHeadCoordinates[0] < 0 ? 
-            this.snakeHeadCoordinates[0] = this.width-1 :
-            null;
+          this.snakeHeadCoordinates[0] < 0
+            ? this.snakeHeadCoordinates[0] = this.width - 1
+            : null;
           break;
         case directions.UP:
           this.snakeHeadCoordinates[1]--;
-          this.snakeHeadCoordinates[1] < 0 ? 
-            this.snakeHeadCoordinates[1] = this.height-1 :
-            null;
+          this.snakeHeadCoordinates[1] < 0
+            ? this.snakeHeadCoordinates[1] = this.height - 1
+            : null;
           break;
         case directions.DOWN:
           this.snakeHeadCoordinates[1]++;
-          this.snakeHeadCoordinates[1] == this.height ? 
-            this.snakeHeadCoordinates[1] = 0 :
-            null;
+          this.snakeHeadCoordinates[1] === this.height
+            ? this.snakeHeadCoordinates[1] = 0
+            : null;
           break;
         default:
           return;
       }
 
-      const nextCellValue = this.worldMap.cells[this.snakeHeadCoordinates[0]][this.snakeHeadCoordinates[1]]
+      const nextCellValue = this.worldMap.cells[this.snakeHeadCoordinates[0]][this.snakeHeadCoordinates[1]];
       if (nextCellValue > 0) {
-        this.gameInit()
-      } else if (nextCellValue == 'x') {
+        this.gameInit();
+      } else if (nextCellValue === 'x') {
         this.snakeLength++;
         this.placeApple();
-        this.rate = this.rate * 0.95
+        this.rate *= 0.95;
       }
 
       this.worldMap.setCell(this.snakeHeadCoordinates[0], this.snakeHeadCoordinates[1], this.snakeLength);
       this.worldMap.draw();
-      
+
       this.lastTime = currentTime - (delta % this.rate);
     }
   }
+
   placeApple() {
     const x = Math.floor(Math.random() * this.width);
     const y = Math.floor(Math.random() * this.height);
-    const worldCellValue = this.worldMap.cells[x][y]
-    if (worldCellValue == 0) {
+    const worldCellValue = this.worldMap.cells[x][y];
+    if (worldCellValue === 0) {
       this.worldMap.setCell(x, y, 'x');
       return;
     }
     this.placeApple();
   }
+
   placeSnake() {
-    const headX = Math.round(this.width/2)-1;
-    const headY = Math.round(this.height/2)-1;
+    const headX = Math.round(this.width / 2) - 1;
+    const headY = Math.round(this.height / 2) - 1;
     this.snakeHeadCoordinates = [headX, headY];
 
-    for (let i=0; i < this.snakeLength; i++) {
-      let x = headX  - i;
-      let y = headY;
-      let value = this.snakeLength - i;
+    for (let i = 0; i < this.snakeLength; i++) {
+      const x = headX - i;
+      const y = headY;
+      const value = this.snakeLength - i;
       this.worldMap.setCell(x, y, value);
     }
   }
+
   handleKeypress(event) {
     event.preventDefault();
     switch (event.key) {
       case 'ArrowUp':
-          if (this.snakeDirection == directions.DOWN) {
-            break
-          }
-          this.snakeDirection = directions.UP;
+        if (this.snakeDirection === directions.DOWN) {
           break;
+        }
+        this.snakeDirection = directions.UP;
+        break;
       case 'ArrowDown':
-          if (this.snakeDirection == directions.UP) {
-            break;
-          }
-          this.snakeDirection = directions.DOWN;
+        if (this.snakeDirection === directions.UP) {
           break;
+        }
+        this.snakeDirection = directions.DOWN;
+        break;
       case 'ArrowLeft':
-          if (this.snakeDirection == directions.RIGHT) {
-            break;
-          }
-          this.snakeDirection = directions.LEFT;
+        if (this.snakeDirection === directions.RIGHT) {
           break;
+        }
+        this.snakeDirection = directions.LEFT;
+        break;
       case 'ArrowRight':
-          if (this.snakeDirection == directions.LEFT) {
-            break;
-          }
-          this.snakeDirection = directions.RIGHT;
+        if (this.snakeDirection === directions.LEFT) {
           break;
+        }
+        this.snakeDirection = directions.RIGHT;
+        break;
       default:
-          return;
+        return;
     }
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-import Game from './game'
+import Game from './game';
 
-const ctx = document.getElementById("myCanvas").getContext("2d");
+const ctx = document.getElementById('myCanvas').getContext('2d');
 
-let game = new Game(30, 15, ctx);
+const game = new Game(30, 15, ctx);
 
-document.addEventListener('keydown', (e) => game.handleKeypress(e), false);
+document.addEventListener('keydown', e => game.handleKeypress(e), false);
 
-game.gameLoop()
+game.gameLoop();
